9285230d7dd8b5a2b0443254a207d82ec9784908
Ilya Malinowski <malinowski221106@gmail.com>
2021-08-11 18:10:18 +0300

    mockup

--- app/src/main/java/com/example/attractionscompose/MainActivity.kt
+++ app/src/main/java/com/example/attractionscompose/MainActivity.kt
@@ -4,18 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Black
+import androidx.compose.ui.graphics.Color.Companion.Cyan
+import androidx.compose.ui.graphics.Color.Companion.White
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.attractionscompose.ui.theme.AttractionsComposeTheme
-import com.example.attractionscompose.ui.theme.Shapes
+import java.lang.StringBuilder
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,43 +40,29 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Test(name: String) {
-    var count by remember { mutableStateOf(0) }
-    Box(modifier = Modifier
+    Column(modifier = Modifier
         .fillMaxWidth()
-        .fillMaxHeight(),) {
+        .fillMaxHeight()) {
         Text(
-            modifier = Modifier.align(Alignment.Center)
-                .clickable { count += 1 }
-                .clip(Shapes.medium)
-                .background(MaterialTheme.colors.primary)
-                .padding(15.dp),
-            text = "$name $count",
-            fontSize = 16.sp
+            text = name,
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center,
+            color = if(isSystemInDarkTheme()) White else Black
         )
-    }
-    val onPopupDismissed = {count = 1}
-    if (count % 10 == 0) {
-        // Predefined composable provided by the material implementations of Jetpack Compose. It
-        // shows a simple alert dialog on the screen if this code path is executed (i.e showPopup
-        // variable is true)
-        AlertDialog(
-            onDismissRequest = onPopupDismissed,
-            text = {
-                Text("Congratulations! You just clicked the text successfully")
-            },
-            confirmButton = {
-                // Button is a pre-defined Material Design implementation of a contained button -
-                // https://material.io/design/components/buttons.html#contained-button.
-                Button(
-                    onClick = onPopupDismissed
-                ) {
-                    // The Button composable allows you to provide child composables that inherit
-                    // this button functionality.
-                    // The Text composable is pre-defined by the Compose UI library; you can use this
-                    // composable to render text on the screen
-                    Text(text = "Ok")
-                }
-            })
+        Box(
+            modifier = Modifier
+                .width(300.dp)
+                .height(300.dp)
+                .align(CenterHorizontally)
+                .clip(RoundedCornerShape(10.dp))
+                .background(Cyan)
+        )
+        Text(
+            text = "$name ".repeat(100),
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center,
+            color = if(isSystemInDarkTheme()) White else Black
+        )
     }
 }
 
