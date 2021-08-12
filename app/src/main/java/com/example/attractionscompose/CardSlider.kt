02dddb759775ae9c4faa50159919e49ddbfcee77
Ilya Malinowski <malinowski221106@gmail.com>
2021-08-12 13:12:06 +0300

    Cards Slider word in progress

---
+++ app/src/main/java/com/example/attractionscompose/CardSlider.kt
@@ -0,0 +1,65 @@
+package com.example.attractionscompose
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.attractionscompose.ui.theme.Purple500
+
+@Composable
+fun CardSlider(modifier: Modifier = Modifier, cards:List<Color>){
+    val listState = rememberLazyListState()
+    Column(Modifier.fillMaxWidth()){
+        LazyRow {
+            items(cards.size) { index ->
+                Box(
+                    modifier = modifier
+                        .padding(horizontal = 50.dp)
+                        .width(300.dp)
+                        .height(300.dp)
+                        .clip(RoundedCornerShape(10.dp))
+                        .background(cards[index])
+                )
+            }
+        }
+        DotsNavigation(
+            Modifier.align(CenterHorizontally),
+            cards.size,
+            listState.firstVisibleItemIndex
+        )
+    }
+}
+
+@Composable
+private fun DotsNavigation(modifier: Modifier = Modifier, size: Int, cur : Int){
+    Row (
+        modifier.padding(10.dp)
+    ){
+        for (i in 0 until size) {
+            Box(
+                modifier = Modifier
+                    .padding(horizontal = 2.dp)
+                    .size(5.dp)
+                    .clip(CircleShape)
+                    .background(if(i == cur) Color.Red else Color.DarkGray)
+            )
+        }
+    }
+}
+
+@Preview()
+@Composable
+fun CardSliderPreview(){
+    CardSlider(cards = listOf(Color.Yellow, Color.Cyan, Purple500, Color.Red, Color.Blue, Color.Green))
+}
--- app/src/main/java/com/example/attractionscompose/MainActivity.kt
+++ app/src/main/java/com/example/attractionscompose/MainActivity.kt
@@ -3,34 +3,21 @@ package com.example.attractionscompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Color.Companion.Black
-import androidx.compose.ui.graphics.Color.Companion.Blue
-import androidx.compose.ui.graphics.Color.Companion.Cyan
-import androidx.compose.ui.graphics.Color.Companion.Green
-import androidx.compose.ui.graphics.Color.Companion.Red
 import androidx.compose.ui.graphics.Color.Companion.White
-import androidx.compose.ui.graphics.Color.Companion.Yellow
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.attractionscompose.ui.theme.AttractionsComposeTheme
 import com.example.attractionscompose.ui.theme.Purple500
-import java.lang.StringBuilder
 
+val colors = listOf(Color.Yellow, Color.Cyan, Purple500, Color.Red, Color.Blue, Color.Green)
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,35 +25,25 @@ class MainActivity : ComponentActivity() {
             AttractionsComposeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Test("Android")
+                    StepScreen(cards = colors)
                 }
             }
         }
     }
 }
-val colors = listOf(Yellow, Cyan, Purple500, Red, Blue, Green)
+
 @Composable
-fun Test(name: String) {
+fun StepScreen(name: String = "Android", cards : List<Color>) {
     Column(modifier = Modifier.fillMaxSize()) {
         Text(
             text = name,
             modifier = Modifier.align(CenterHorizontally),
             color = if(isSystemInDarkTheme()) White else Black
         )
-        LazyRow {
-            items(100) { index ->
-                Box(
-                    modifier = Modifier
-                        .width(300.dp)
-                        .height(300.dp)
-                        .align(CenterHorizontally)
-                        .clip(RoundedCornerShape(10.dp))
-                        .background(colors[index % colors.size])
-                )
-                Spacer(modifier = Modifier
-                    .width(100.dp))
-            }
-        }
+        CardSlider(
+            Modifier.align(CenterHorizontally),
+            colors
+        )
         Text(
             text = "$name ".repeat(100),
             modifier = Modifier.align(CenterHorizontally),
@@ -76,13 +53,18 @@ fun Test(name: String) {
     }
 }
 
-@Preview(showBackground = true)
+@Preview(
+    showSystemUi = true
+)
 @Composable
 fun DefaultPreview() {
     AttractionsComposeTheme {
         // A surface container using the 'background' color from the theme
+        val colors = mutableListOf<Color>().apply {
+            for (i in 1..10) this.addAll(colors)
+        }
         Surface(color = MaterialTheme.colors.background) {
-            Test("Android")
+            StepScreen(cards = colors)
         }
     }
 }
